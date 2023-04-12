dc560520b5983e95a85632beb828ba2ba52fa739
scarletbone3 <dmitry.pronckin2013@yandex.ru>
2023-04-12 23:21:20 +0300

    Test coverage

--- src/tests/solution.test.ts
+++ src/tests/solution.test.ts
@@ -10,7 +10,7 @@ test('No solution', () => {
 
 test('a equal zero', () => {
   const a = 0;
-  const b = 1;
+  const b = 2;
   const c = 2;
   const solution = quadraticEquationSolution(a, b, c);
   expect(solution).toBeNull();
